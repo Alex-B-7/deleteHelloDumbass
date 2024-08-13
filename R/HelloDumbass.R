870ec5958b218596482c87be16975df8f9adb719
Alex Boyle <abboyle@ksu.edu>
2024-08-13 11:04:31 -0500

    Rename .R file

--- R/HelloDumbass.R
+++ R/HelloDumbass.R
@@ -1,7 +1,7 @@
 # Hello, Toby!
 #
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
+# This is an example function named 'fHelloDumbass'
+# which prints 'Hello, Toby'.
 #
 # You can learn more about package authoring with RStudio at:
 #
